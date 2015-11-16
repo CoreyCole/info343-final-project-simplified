01771c2ddf8456f08e8e5a0932a87031488524f5
CoreyCole <coreyleoc@gmail.com>
2015-11-15 18:48:17 -0800

    added protractor test for starting work - #6

--- test/sign_up-spec.js
+++ test/sign_up-spec.js
@@ -1,4 +1,10 @@
 /* Test script for the sign up page app */
 describe('the sign up page app', function() {
+    beforeEach(function() {
+        browser.get('http://localhost:8000/');
+    });
 
+    it('must have the proper page title', function() {
+        expect(browser.getTitle()).toEqual('Sign Up');
+    });
 });
