84bcf4cf416d22b9a44927b054ce55d8cb7c2c67
Memie Huang <memie.huang@gmail.com>
2015-11-17 09:58:16 -0800

    resolves #11

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@
  ui.router -> ui routing
  */
 
-angular.module('BellhappApp', ['ui.router'])
+angular.module('BellhappApp', ['ui.router', 'firebase'])
     .config(function($stateProvider, $urlRouterProvider) {
         $stateProvider
             .state('sign-up', {
@@ -16,6 +16,33 @@ angular.module('BellhappApp', ['ui.router'])
         //default if something weird is typed in
         $urlRouterProvider.otherwise('/sign-up');
     })
-    .controller('SignUpController', function($scope) {
+    .controller('SignUpController', function($scope, $firebaseArray) {
+        $scope.user = {};
+
+        $scope.justLetters = true;
+
+        if(!isNaN($scope.user.fname) || !isNaN($scope.user.lname)){
+            $scope.justLetters = false;
+        }
+
+        //storing into firebase
+        var ref = new Firebase('https://info343-project.firebaseio.com/');
+        $scope.form = $firebaseArray(ref);
+
+        $scope.saveUser = function() {
+
+            $scope.user.$add({
+                email: $scope.user.email,
+                fname: $scope.user.fname,
+                lname: $scope.user.lname,
+                birthdate: $scope.user.birthdate,
+                password: $scope.user.password1
+            });
+            $scope.user = {};
+            localStorage.clear();
+        };
+
+
+
 
     });
--- test/sign_up-spec.js
+++ test/sign_up-spec.js
@@ -1,10 +1,54 @@
 /* Test script for the sign up page app */
 describe('the sign up page app', function() {
+
+    var fName = element(by.id('fnameInp'));
+    var fNameError = element(by.id('fNameError'))
+    var fNamePatternError = element(by.id('fNamePatternError'));
+
+    var lName = element(by.id('lnameInp'));
+    var lNameError = element(by.id('lNameError'));
+    var lNamePatternError = element(by.id('lNamePatternError'));
+
+    var birthdate = element(by.id('birthdateInp'))
+
+
     beforeEach(function() {
         browser.get('http://localhost:8000/');
     });
 
     it('must have the proper page title', function() {
         expect(browser.getTitle()).toEqual('Sign Up');
+    });
+
+    it('must enter first name', function(){
+        fName.click();
+        lName.click();
+        expect(fNameError.isPresent()).toEqual(true);
+        fName.click();
+        fName.sendKeys('Memie');
+        expect(fNameError.isPresent()).toEqual(false);
+    });
+
+    it('must enter last name', function(){
+        lName.click();
+        birthdate.click();
+        expect(lNameError.isPresent()).toEqual(true);
+        lName.click();
+        lName.sendKeys('Huang');
+        expect(lNameError.isPresent()).toEqual(false);
+    });
+
+    it('first name should be valid', function(){
+        expect(fNamePatternError.isPresent()).toEqual(false);
+        fName.sendKeys('123');
+        lName.click();
+        expect(fNamePatternError.isPresent()).toEqual(true);
+    });
+
+    it('last name should be valid', function(){
+        expect(lNamePatternError.isPresent()).toEqual(false);
+        lName.sendKeys('123');
+        fName.click();
+        expect(lNamePatternError.isPresent()).toEqual(true);
     });
 });
