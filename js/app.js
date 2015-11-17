7058a3a90d332efe89bb77296e2372cee7a85b28
Memie Huang <memie.huang@gmail.com>
2015-11-17 10:10:45 -0800

    resolves #11

--- js/app.js
+++ js/app.js
@@ -17,32 +17,5 @@ angular.module('BellhappApp', ['ui.router', 'firebase'])
         $urlRouterProvider.otherwise('/sign-up');
     })
     .controller('SignUpController', function($scope, $firebaseArray) {
-        $scope.user = {};
-
-        $scope.justLetters = true;
-
-        if(!isNaN($scope.user.fname) || !isNaN($scope.user.lname)){
-            $scope.justLetters = false;
-        }
-
-        //storing into firebase
-        var ref = new Firebase('https://info343-project.firebaseio.com/');
-        $scope.form = $firebaseArray(ref);
-
-        $scope.saveUser = function() {
-
-            $scope.user.$add({
-                email: $scope.user.email,
-                fname: $scope.user.fname,
-                lname: $scope.user.lname,
-                birthdate: $scope.user.birthdate,
-                password: $scope.user.password1
-            });
-            $scope.user = {};
-            localStorage.clear();
-        };
-
-
-
 
     });
