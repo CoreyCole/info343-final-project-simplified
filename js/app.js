514abdc4bb2f22772892b0d6e453e50867f226ed
arnavd96 <arnavdubey96@gmail.com>
2015-11-16 18:03:52 -0800

    resolves #8 and resolves #9

--- js/app.js
+++ js/app.js
@@ -16,6 +16,32 @@ angular.module('BellhappApp', ['ui.router'])
         //default if something weird is typed in
         $urlRouterProvider.otherwise('/sign-up');
     })
+    .directive('checkBirthdate', function() {
+        return {
+            require: 'ngModel',
+            link: function(scope, elem, attrs, controller) {
+                controller.$validators.checkBirthdate = function(modelValue) {
+                    var userDate = Date.parse(modelValue);
+                    if (!isNaN(userDate)) {
+                        var thirteen = 13 * 365 * 24 * 3600 * 1000;
+                        return (userDate <= Date.now() - thirteen);
+                    }
+                    return false;
+                }
+            }
+        }
+    })
+    .directive('passwordsMatch', function() {
+        return {
+            require: 'ngModel',
+            link: function(scope, elem, attrs, controller) {
+                controller.$validators.passwordsMatch = function(modelValue) {
+                    return modelValue == scope.$eval(attrs.passwordsMatch);
+                };
+            }
+        }
+    })
     .controller('SignUpController', function($scope) {
+
 
     });
